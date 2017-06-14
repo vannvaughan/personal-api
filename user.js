a37596f550df4a9ba946287da8d876ddc9e79b73
vannvaughan <vann.vaughan@yahoo.com>
2017-06-14 14:06:00 -0500

    Updated

---
+++ user.js
@@ -0,0 +1,58 @@
+var user = {
+
+  restaurants: [
+    {
+      name: 'Flemings',
+      type: 'Steak House',
+      rating: 10
+    },
+    {
+      name: 'Royale Magnificent',
+      type: 'Burgers',
+      rating: 9.5
+    },
+    {
+      name: 'Chop House Burgers',
+      type: 'Burgers',
+      rating: 6
+    }
+    {
+      name: 'Bowls and Tacos',
+      type: 'Tacos',
+      rating: 9
+    }
+    {
+      name: 'Mexi-go',
+      type: 'Mexican',
+      rating: 7.5
+    }
+    {
+      name: 'Pluckers',
+      type: 'American',
+      rating: 6
+    }
+    {
+      name: 'Dillas',
+      type: 'Quesadillas',
+      rating: 8
+    }
+    {
+      name: 'Kellers',
+      type: 'Burgers',
+      rating: 10
+    }
+    {
+      name: 'Perrys',
+      type: 'Steak House',
+      rating: 10
+    }
+    {
+      name: 'Cafe Brazil',
+      type: 'Brazilian',
+      rating: 1
+    }
+  ]
+}
+
+module.export = user;
+
