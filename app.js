e66163e3662cd53cd670ad6405b7e195409bddf4
vannvaughan <vann.vaughan@yahoo.com>
2017-06-14 14:19:13 -0500

    Completed step 1-2.

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 var express = require('express');
 var app = express();
+
 var bodyParser = require('body-parser');
 var user = require('./user');
-
+var mainCtrl = require('./controllers/mainCtrl');
+var middleware = require('./controllers/middleware');
 
 app.use(bodyParser.json());
+
+app.use(middleware.addHeaders);
+
+
+
+
+app.listen(3000, function(){
+    console.log('THUUGG LIIIFFEEE!!!!')
+});
---
+++ controllers/middleware.js
@@ -0,0 +1,19 @@
+var user = require('../user');
+
+module.export = {
+
+    addHeader: function(req, res){
+        res.status(200).set({
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS, GET, POST, PUT',
+            'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+            'X-XSS-Protection': '1; mode=block',
+            'X-Frame-Options': 'SAMEORIGIN',
+            'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+        });
+    
+        next();
+    };
+
+};
